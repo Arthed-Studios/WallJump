f096bc4a973c54e0c5843e1a8a3d63c3c0bb892d
Styro457 <raduursache6@gmail.com>
2021-06-14 21:17:43 +0300

    1.8 Compatibility

--- src/main/java/me/arthed/walljump/utils/VelocityUtils.java
+++ src/main/java/me/arthed/walljump/utils/VelocityUtils.java
@@ -6,6 +6,8 @@ import org.bukkit.util.Vector;
 public class VelocityUtils {
 
     public static void pushPlayerInFront(Player player, float horizontalPower, float verticalPower) {
+        if(BukkitUtils.isVersionBefore(BukkitUtils.Version.V1_8))
+            verticalPower++;
         Vector velocity = player.getLocation().getDirection().normalize().multiply(horizontalPower);
         velocity.setY(verticalPower);
         player.setVelocity(velocity);
