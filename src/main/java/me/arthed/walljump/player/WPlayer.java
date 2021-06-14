094cd4ae1117fa9068a06b58238393ab0a475435
Styro457 <raduursache6@gmail.com>
2021-06-14 15:37:30 +0300

    Adder permission to wall-jump

--- src/main/java/me/arthed/walljump/player/WPlayer.java
+++ src/main/java/me/arthed/walljump/player/WPlayer.java
@@ -43,7 +43,9 @@ public class WPlayer {
                 remainingJumps == 0 || //player reached jump limit
                 (lastFacing != null && lastFacing.equals(player.getFacing())) || //player is facing the same direction as the last jump
                 (lastJumpLocation != null && player.getLocation().distance(lastJumpLocation) <= config.getDouble("minimumDistance")) ||  //player is too close to the last jump location
-                player.getVelocity().getY() < config.getDouble("maximumVelocity")) //player is falling too fast
+                player.getVelocity().getY() < config.getDouble("maximumVelocity") || //player is falling too fast
+                (config.getBoolean("needPermission") && !player.hasPermission("walljump.use")) //player does not have the permission to wall-jump
+        )
 
             return;
 
